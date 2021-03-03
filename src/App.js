33905ce6a7fab250d36eafa718c7eb1c5406e9ed
Luciano Cancinos <lucianocancinos99@gmail.com>
2021-03-03 03:19:33 -0300

    producto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Fragment} from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+import Productocard from './components/producto/Producto';
 
 function App() {
   return (
       <div>
-        <NavBar></NavBar>      
+        <NavBar></NavBar>
+        <Productocard></Productocard>      
       </div>
   );
 }
